007fb6d0eae7a786c2fc6faf87f0b78e67be3e0e
Dayoung Jeong <58131560+day0ung@users.noreply.github.com>
2023-05-22 14:52:37 +0900

    Update 윤년.py

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -7,3 +7,9 @@ elif( year % 4 == 0 ) and (year % 400 == 0):
     print(1)
 else:
     print(0)
+    
+# 이게더 좋은방법인것같음
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#     print(1)
+# else:
+#    print(0)
